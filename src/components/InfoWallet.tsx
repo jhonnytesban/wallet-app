78102da21b3c0d586cab81d2e392591da45663d4
Jonathan <jonathanestebangalan@gmail.com>
2022-04-30 09:15:09 +0200

    Added styles infoWallet component

--- src/components/InfoWallet.tsx
+++ src/components/InfoWallet.tsx
@@ -40,6 +40,7 @@ const InfoWallet = () => {
 
 const ContainerInfoWallet = styled.div`
   width: 80%;
+  max-width: 400px;
   border-radius: 10px;
   box-shadow: 0px 2px 8px -4px black;
   margin: auto;
@@ -50,13 +51,23 @@ const ContainerInfoWallet = styled.div`
 const InfoAccountWallet = styled.div`
   display: flex;
   align-items: center;
+  margin-bottom: 1rem;
 
   img {
-    width: 30px;
+    width: 35px;
   }
 
   div {
-    margin-left: 10px;
+    margin-left: 20px;
+
+    p:nth-child(1) {
+      font-size: .8rem;
+      font-weight: 300;
+    }
+    p:nth-child(2) {
+      font-size: 1.5rem;
+      font-weight: 800;
+    }
   }
 `
 
@@ -65,6 +76,13 @@ const InfoMovementWallet = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    font-size: .9rem;
+    font-weight: 300;
+    margin-bottom: 5px;
+  }
+
+  div:nth-child(1){
+    margin-bottom: 10px;
   }
 `
 
